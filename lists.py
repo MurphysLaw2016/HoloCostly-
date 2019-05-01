0bc35b56bfa3b1dfb8bbfcf5a6fcb8a75b497bc2
MurphysLaw2016 <44299918+MurphysLaw2016@users.noreply.github.com>
2019-05-01 15:00:39 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,73 @@
+"""
+
+Zweck:
+WW2
+Nazi-Todeslager schauen auf
+Eingang/Ausgabe:
+
+"""
+Lager = [
+    "These are the name of the death camps."
+
+    "Auschwitz–Birkenau",
+    "Treblinka",
+    "Bełżec",
+    "Chełmno",
+    "Sobibór",
+    "Majdanek",
+    "Maly Trostinets",
+    "Sajmište",
+]
+
+Tod = [
+    "These are the amomount of deaths of jewish citizens who were killed.",
+    "Auschwitz–Birkenau: the death count was 1,100,000",
+    "Treblinka: the death count was 800,000",
+    "Bełżec: the death count was 600,000",
+    "Chełmno: the death count was 320,000",
+    "Sobibór: the death count was 250,000",
+    "Majdanek: the death count was at least 80,000",
+    "Maly Trostinets: the death count was 65,000",
+    "Sajmište: the death count was 23,000",
+
+]
+Land = [
+    "This is where the death camps were located."
+
+    "Auschwitz–Birkenau was in Poland",
+    "Treblinka was in Poland",
+    "Bełżec was in Poland",
+    "Chełmno was in Poland",
+    "Sobibór was in Poland",
+    "Majdanek was in Poland",
+    "Maly Trostinets was in	Belarus",
+    "Sajmište was in Serbia",
+]
+Betriebszeit = [
+    "These are the operational lengths of the death camps."
+
+    "Auschwitz–Birkenau: May 1940 – January 1945",
+    "Treblinka: 23 July 1942 – 19 October 1943",
+    "Bełżec: 17 March 1942 – end of June 1943",
+    "Chełmno: 8 December 1941 – March 1943,June 1944 – 18 January 1945",
+    "Sobibór: 16 May 1942 – 17 October 1943",
+    "Majdanek: 1 October 1941 – 22 July 1944 ",
+    "Maly Trostinets: Middle of 1941 to 28 June 1944",
+    "Sajmište: 28 October 1941 – July 1944",
+]
+Gaskammer = [
+    "You chose one of the interesting facts!Dr Albert Widmann was the man who was given credit for the development "
+    "of gas chambers, and the first gas van manufactured in Berlin, was used by the Lange Commando between 21 May and "
+    "8 June 1940 at the Soldau concentration camp in occupied Poland, to kill 1,558 "
+    "mental patients delivered from sanatoria."
+]
+
+Das_verstecken_Juden = [
+    "You chose one of the interesting facts! "
+    "The Nazis attempted to either partially or completely dismantle the extermination camps in order to hide "
+    "any evidence that people had been murdered there. This was an attempt to conceal not only the extermination "
+    "process but also the buried remains. As a result of the secretive Sonderaktion 1005, the camps were dismantled by "
+    "commandos of condemned prisoners, their records were destroyed, and the mass graves were dug up. "
+    "Some extermination camps that remained uncleared of evidence were liberated by Soviet troops, "
+    "who followed different standards of documentation and openness than the Western allies did."
+]
